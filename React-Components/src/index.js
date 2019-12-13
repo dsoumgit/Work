c191ea91612aa76b96e90debe52d298d7285ac31
darasoumgit <dara.soumgit@gmail.com>
2019-12-12 22:28:25 -0500

    created four cards in HTML and CSS

--- React-Components/src/index.js
+++ React-Components/src/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; 
 
+//import Hero from '../components/Hero/hero';
+import FourCard from '../components/Four-card-section/fourCard';
+
 class App extends Component {
     render() {
         return(
-            <div>Checking...</div>
+            <FourCard />
         )
     }
 }
--- React-Components/webpack.config.js
+++ React-Components/webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
                 use: { 
                     loader: 'html-loader'
                 }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
             }
         ]
     },
