8f4df90710b5e31b0a5538a56981ea049a382f37
darasoumgit <dara.soumgit@gmail.com>
2020-01-09 09:26:16 -0500

    fixed the layout on small devices for navigation menu

--- Projects/src/Portfolio/components/Navigation/header.js
+++ Projects/src/Portfolio/components/Navigation/header.js
@@ -9,25 +9,30 @@ const Header = () => {
                 <div className="site-logo">
                     <h1>D.S</h1>
                 </div>
-                <nav>
-                    <ul className="nav-list">
-                        <li className="nav-list__item">
-                            <a href="#about-me" className="nav-link">About</a>
-                        </li>
-                        <li className="nav-list__item">
-                            <a href="#portfolio" className="nav-link">Portfolio</a>
-                        </li>
-                        <li className="nav-list__item">
-                            <a href="#contact-me" className="nav-link">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div className="nav">
+                    <nav>
+                        <ul className="nav-list">
+                            <li className="nav-list__item">
+                                <a href="#about-me" className="nav-link">About</a>
+                            </li>
+                            <li className="nav-list__item">
+                                <a href="#portfolio" className="nav-link">Portfolio</a>
+                            </li>
+                            <li className="nav-list__item">
+                                <a href="#contact-me" className="nav-link">Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>    
             </header>
             <div className="hero__background">
                 <div className="hero__text">
                     <h1 className="heading-1">Dara Soumgit</h1>
                     <h2 className="heading-2 mb-large">Front End Developer</h2>
-                    <button className="btn">Read More <span className="hero__text--more">&darr;</span></button>
+                    <button className="btn">
+                        <a href="#about-me" className="btn-link">Read More </a> 
+                        <span className="hero__text--more">&darr;</span>
+                    </button>
                 </div>
             </div>
         </div>
