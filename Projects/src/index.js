72e09b286c831bd652c93fdc155286335c8eaf40
darasoumgit <dara.soumgit@gmail.com>
2020-01-16 21:39:39 -0500

    created a Blog app to use Redux and redux-thunk

--- Projects/src/index.js
+++ Projects/src/index.js
@@ -1,32 +1,38 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
 // using global SASS files 
 //import '../Shared/main.scss';
 //import Home from './Portfolio/components/Home/home';
 
-/* Youtube app
- import Home from './Youtube/components/Home';
-*/
 
-/* Photowall app 
- import Home from './Photowall/components/Home';
- Note: For Photowall app, make sure to wrap the Home within browser router
-*/
+// Youtube app
+//import Home from './Youtube/components/Home';
 
-// Songs app using Redux with React 
+
+// Photowall app 
+import { BrowserRouter } from 'react-router-dom'; 
+import Home from './Photowall/components/Home';
+// Note: For Photowall app, make sure to wrap the Home within browser router
+
+
+/* Songs app using Redux with React 
 import Home from './Songs/components/Home';
 // Provider object
 import { Provider } from 'react-redux'; 
 import { createStore } from 'redux'; 
-import reducers from './Songs/reducers/index';
+import reducers from './Songs/reducers/index'; 
+
+    <Provider store={createStore(reducers)}>
+          <Home />
+        </Provider> 
+*/
 
   const App = () => {
   
       return(
-        <Provider store={createStore(reducers)}>
+        <BrowserRouter>
           <Home />
-        </Provider>
+        </BrowserRouter>
       )
   }
 
