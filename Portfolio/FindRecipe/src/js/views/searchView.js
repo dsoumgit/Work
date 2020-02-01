157c38d6793ab9f8f667282586f240dffc7c432d
darasoumgit <dara.soumgit@gmail.com>
2020-02-01 10:24:42 -0500

    added a function to limit the title recipe

--- Portfolio/FindRecipe/src/js/views/searchView.js
+++ Portfolio/FindRecipe/src/js/views/searchView.js
@@ -17,6 +17,30 @@ export const clearResults = () => {
     elements.searchResultList.innerHTML = ''; 
 }
 
+// this function is to limit the title, using default parameter in ES6  
+const limitRecipeTitle = (title, limit = 17) => {
+    const output = [];
+    // check the title 
+    if (title.length >= limit) {
+        // split the string and use reduce to count each length 
+        title.split('').reduce((acc, cur) => {
+            // check if accumulate and current are less than or equal to limit 
+            if (acc + cur.length <= limit) {
+                // store each element a new array 
+                output.push(cur);
+            }   
+            // return the acc and current length 
+            return acc + cur.length;
+        }, 0);
+
+        // return the result and use join() to combine all elements and add dots after it 
+        return `${output.join('')} ...`;  //output.join('') + '...';
+    }
+
+    // return title 
+    return title; 
+}
+
 const renderRecipe = recipe => {
     // use template string to return each item 
     const markup = `
@@ -26,7 +50,7 @@ const renderRecipe = recipe => {
                         <img src="${recipe.image}" alt="Test">
                     </figure>
                     <div class="results__data">
-                        <h4 class="results__name">${recipe.title}</h4>
+                        <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                         <p class="results__author">Item #: ${recipe.id}</p>
                     </div>
                 </a>
