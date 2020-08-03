414d811825481d94ecd888cc81c249f8fd1560d3
darasoumgit <dara.soumgit@gmail.com>
2020-08-03 15:46:10 -0400

    added sass loader in webpack

--- Projects/webpack.config.js
+++ Projects/webpack.config.js
@@ -20,8 +20,12 @@ module.exports = {
                 ],
             },*/
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.(png|svg|jpg|gif|pdf)$/,
